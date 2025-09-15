3b29e991b9c53fe19b64aca4f43dff86abd17aed
James Mark Chan <jamesmarkchan@gmail.com>
2025-09-14 19:52:04 -0700

    #73 initial datamodel refactor

--- src/jdiskmark/BenchmarkOperation.java
+++ src/jdiskmark/BenchmarkOperation.java
@@ -1,17 +1,18 @@
 
 package jdiskmark;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.text.DecimalFormat;
@@ -25,18 +26,18 @@ import java.util.List;
  * A read or write benchmark
  */
 @Entity
-@Table(name="Benchmark")
+@Table(name="BenchmarkOperation")
 @NamedQueries({
-@NamedQuery(name="Benchmark.findAll",
-    query="SELECT b FROM Benchmark b JOIN FETCH b.operations")
+@NamedQuery(name="BenchmarkOperation.findAll",
+    query="SELECT d FROM BenchmarkOperation d")
 })
-public class Benchmark implements Serializable {
+public class BenchmarkOperation implements Serializable {
     
     static final DecimalFormat DF = new DecimalFormat("###.##");
     static final DecimalFormat DFT = new DecimalFormat("###");
     static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     
-    public enum BenchmarkType {
+    public enum IOMode {
         READ {
             @Override
             public String toString() { return "Read"; }
@@ -44,10 +45,17 @@ public class Benchmark implements Serializable {
         WRITE {
             @Override
             public String toString() { return "Write"; }
-        },
-        READ_WRITE {    
+        }
+    }
+
+    public enum BlockSequence {
+        SEQUENTIAL {
             @Override
-            public String toString() { return "Read & Write"; }
+            public String toString() { return "Sequential"; }
+        },
+        RANDOM {
+            @Override
+            public String toString() { return "Random"; }
         }
     }
     
@@ -57,30 +65,30 @@ public class Benchmark implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
     
-    // system data
-    @Column
-    String os;
-    @Column
-    String arch;
-    @Column
-    String processorName;
-    
-    // drive info
-    @Column
-    String driveModel = null;
-    @Column
-    String partitionId;      // on windows the drive letter
-    @Column
-    long percentUsed;
-    @Column
-    double usedGb;
-    @Column
-    double totalGb;
+    // This is the foreign key relationship
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "benchmark_id") // Specifies the foreign key column
+    private Benchmark benchmark;
     
     // benchmark parameters
     @Column
-    BenchmarkType benchmarkType;
-
+    IOMode ioMode;
+    @Column
+    BlockSequence blockOrder;
+    @Column
+    int numBlocks = 0;
+    @Column
+    int blockSize = 0;
+    @Column
+    int numSamples = 0;
+    @Column
+    long txSize = 0;
+    @Column
+    int numThreads = 1;
+    // NEW: whether write-sync was enabled for this run (only meaningful for WRITE; may be null for READ)
+    @Column
+    Boolean writeSyncEnabled;
+    
     // timestamps
     @Convert(converter = LocalDateTimeAttributeConverter.class)
     @Column(name = "startTime", columnDefinition = "TIMESTAMP")
@@ -88,7 +96,12 @@ public class Benchmark implements Serializable {
     @Convert(converter = LocalDateTimeAttributeConverter.class)
     @Column
     LocalDateTime endTime = null;
-
+    
+    // sample data
+    @Column
+    @Convert(converter = SampleAttributeConverter.class)
+    ArrayList<Sample> samples = new ArrayList<>();
+    
     // results
     @Column
     double bwAvg = 0;
@@ -101,65 +114,58 @@ public class Benchmark implements Serializable {
     @Column
     long iops = 0;
     
-    @OneToMany(mappedBy = "benchmark", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<BenchmarkOperation> operations = new ArrayList<>();
-    
-    public List<BenchmarkOperation> getOperations() {
-        return operations;
-    }
-    
-    // get the first operation of that type
-    public BenchmarkOperation getOperation(BenchmarkOperation.IOMode mode) {
-        for (BenchmarkOperation operation : operations) {
-            if (operation.ioMode == mode) {
-                return operation;
-            }
-        }
-        return null;
-    }
-    
     @Override
     public String toString() {
-        return "Benchmark(" + benchmarkType + "):  bw: " + bwAvg;
+        return "BenchmarkOp(" + ioMode + "," + blockOrder + "): " + numSamples + " bw avg: " + bwAvg;
     }
     
-    public Benchmark() {
+    public BenchmarkOperation() {
         startTime = LocalDateTime.now();
     }
     
-    Benchmark(BenchmarkType type) {
+    BenchmarkOperation(IOMode type, BlockSequence order) {
         startTime = LocalDateTime.now();
-        benchmarkType = type;
+        ioMode = type;
+        blockOrder = order;
     }
     
     // basic getters and setters
-    
+    public Benchmark getBenchmark() {
+        return benchmark;
+    }
+    public void setBenchmark(Benchmark benchmark) {
+        this.benchmark = benchmark;
+    }    
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
-    public String getDriveInfo() {
-        return driveModel + " - " + partitionId + ": " + getUsageTitleDisplay();
+    
+    public String getModeDisplay() {
+        // Show "Write*" when write-sync was enabled for a WRITE run
+        if (ioMode == IOMode.WRITE && Boolean.TRUE.equals(getWriteSyncEnabled())) {
+            return "Write*";
+        }
+        return (ioMode == null) ? "" : ioMode.toString(); // "Read", "Write", "Read & Write"
     }
-    public String getUsageTitleDisplay() {
-        return  percentUsed + "% (" + DFT.format(usedGb) + "/" + DFT.format(totalGb) + " GB)";
-    }
-    public String getUsageColumnDisplay() {
-        return percentUsed + "%";
-    }
+
     
     // GH-20 TODO: review should this be synchronized or redone to not be blocking?
-//    public synchronized void add(Sample s) {
-//        samples.add(s);
-//    }
+    public synchronized void add(Sample s) {
+        samples.add(s);
+    }
+    
+    public synchronized ArrayList<Sample> getSamples() {
+        return samples;
+    }
     
     // display friendly methods
     
-//    public String getBlocksDisplay() {
-//        return numBlocks + " (" + blockSize + ")";
-//    }
+    public String getBlocksDisplay() {
+        return numBlocks + " (" + blockSize + ")";
+    }
     
     public String getStartTimeString() {
         return startTime.format(DATE_FORMAT);
@@ -204,6 +210,16 @@ public class Benchmark implements Serializable {
             iops = Math.round(iopsDouble);
         }
     }
+    
+    // NEW: getters/setters for writeSyncEnabled and iops (expose iops too if needed)
+
+    public Boolean getWriteSyncEnabled() {
+        return writeSyncEnabled;
+    }
+
+    public void setWriteSyncEnabled(Boolean writeSyncEnabled) {
+        this.writeSyncEnabled = writeSyncEnabled;
+    }
 
     public long getIops() {
         return iops;
@@ -215,36 +231,26 @@ public class Benchmark implements Serializable {
     
     // utility methods for collection
     
-    static List<Benchmark> findAll() {
+    static List<BenchmarkOperation> findAll() {
         EntityManager em = EM.getEntityManager();
-        return em.createNamedQuery("Benchmark.findAll", Benchmark.class).getResultList();
+        return em.createNamedQuery("Benchmark.findAll", BenchmarkOperation.class).getResultList();
     }
     
     static int deleteAll() {
         EntityManager em = EM.getEntityManager();
         em.getTransaction().begin();
-        int deletedOperationsCount = em.createQuery("DELETE FROM BenchmarkOperation").executeUpdate();
-        int deletedBenchmarksCount = em.createQuery("DELETE FROM Benchmark").executeUpdate();
+        int deletedCount = em.createQuery("DELETE FROM Benchmark").executeUpdate();
         em.getTransaction().commit();
-        return deletedBenchmarksCount;
+        return deletedCount;
     }
     
     static int delete(List<Long> benchmarkIds) {
         EntityManager em = EM.getEntityManager();
         em.getTransaction().begin();
-        
-        // delete the child BenchmarkOperation records.
-        String deleteOperationsJpql = "DELETE FROM BenchmarkOperation bo WHERE bo.benchmark.id IN :benchmarkIds";
-        int deletedOpsCount = em.createQuery(deleteOperationsJpql)
+        String jpql = "DELETE FROM Benchmark b WHERE b.id IN :benchmarkIds";
+        int deletedCount = em.createQuery(jpql)
                 .setParameter("benchmarkIds", benchmarkIds)
                 .executeUpdate();
-        
-        // delete the parent BenchmarkOperation records
-        String deleteBenchmarksJpql = "DELETE FROM Benchmark b WHERE b.id IN :benchmarkIds";
-        int deletedCount = em.createQuery(deleteBenchmarksJpql)
-                .setParameter("benchmarkIds", benchmarkIds)
-                .executeUpdate();
-        
         em.getTransaction().commit();
         return deletedCount;
     }
